1c1e79e9c2ef232398b25d6219e9018b3d89beda
nlasvn <114028830+nlasvn@users.noreply.github.com>
2024-10-06 11:22:04 -0300

    Add files via upload
    better if expression

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,6 @@
 def fib(num):
-    if num == 0:
-        return 0
-    if num == 1:
-        return 1
-    if num == 2:
-        return 1
+    if num <= 1:
+        return num
     return fib(num - 1) + fib(num - 2)
 
 
